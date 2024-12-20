5ed2f4b6e0db29b8a090d3bdf143d36fa76a7082
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-20 14:05:37 +0100

    were getting somewhere

--- client/src/OneBook.js
+++ client/src/OneBook.js
@@ -1,5 +1,15 @@
 import { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
+const GET_BOOK_BY_ID = gql`
+  query Book($id: ID!) {
+    book(id: $id) {
+      id,
+      title,
+      author
+    }
+  }
+`;
+
 function OneBook(){
     const [bookSearched, setBookSearched] = useState(1);
     console.log(bookSearched);
