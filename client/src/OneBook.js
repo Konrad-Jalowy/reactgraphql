6b99315f80adf172eea96bbe0d6c0f98c67ae0a4
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-20 14:03:19 +0100

    were getting somewhere

--- client/src/OneBook.js
+++ client/src/OneBook.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 function OneBook(){
+    const [movieSearched, setMovieSearched] = useState(1);
+    console.log(movieSearched);
     return (
         <>
+        <input type="number" value={movieSearched} onChange={(e) => {
+            setMovieSearched(e.target.value);
+        }} />
         <p>Not implemented yet(onebook)</p>
         </>
     );
