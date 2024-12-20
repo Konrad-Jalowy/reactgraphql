c53bfb86d20c13e20d4a7f448fab058979187260
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-20 13:24:28 +0100

    works

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/graphql",
+  });
   return (
     <div className="App">
       <p>Hello world</p>
